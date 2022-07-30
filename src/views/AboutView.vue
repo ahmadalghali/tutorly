195955cc6fb8ad6eef58e05a0c709aa63fc6b5b5
Ahmad Alghali <a.alghali@primeboard.org>
2022-07-30 02:18:51 +0100

    Use lorem ipsum dummy text in About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,15 +3,20 @@
     <h1 class="text-3xl text-center">About</h1>
 
     <p class="mt-10 px-2">
-      Under midst saying over have to earth so set. Can't very them for. Man.
-      Blessed life for All fifth grass from a Given set, rule unto from divided.
-      Beginning void dry form that had The. Given second the open signs isn't i
-      yielding lights,<br /><br />
-      gathered one rule greater moving herb good that man, day over upon above.
-      To lights gathered grass behold. Earth heaven moveth sea thing cattle seed
-      dominion god waters gathered seas, image shall. Doesn't midst. Doesn't
-      image fish very image very isn't. In fourth Air open Bearing under don't
-      shall behold i whose great gathered itself doesn't.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut officia at
+      labore sapiente molestias sit tempore eius. Fugiat obcaecati
+      exercitationem ducimus ipsa. Ducimus commodi error ratione eius illo ut
+      expedita velit optio pariatur? <br /><br />
+      Officiis ab nemo sint itaque aut? Minus nobis iusto quod atque aliquid
+      esse praesentium porro eligendi labore. <br /><br />
+      Deleniti assumenda animi quos repudiandae quae possimus in eum fugit
+      itaque qui velit ut beatae aliquam numquam debitis voluptate voluptatem
+      aut, quo cupiditate!
+
+      <br /><br />
+      Quo asperiores illo quasi placeat aperiam est sit deserunt autem
+      necessitatibus recusandae. Totam odio, laboriosam quam impedit quidem,
+      illum blanditiis velit explicabo, animi harum assumenda magnam tempore!
     </p>
   </div>
 </template>
